17bf7312f578dff07315166d61bc6f3430bbfce6
goropikari <goropikari56@gmail.com>
2019-08-11 17:15:08 +0900

    implement symbol

--- src/JLLisp.jl
+++ src/JLLisp.jl
@@ -1,5 +1,34 @@
 module JLLisp
 
-greet() = print("Hello World!")
+abstract type T end
+abstract type Atom <: T end
 
-end # module
+module Symbols
+    import ..JLLisp
+
+    struct Symbol_
+        name::JLLisp.T
+        value::JLLisp.T
+        fn::JLLisp.T
+
+        function Symbol_(name::String)
+            x = new()
+            x.name = name
+            x
+        end
+    end
+
+    function symbol_(name::String)
+        get!(symboltable, name, Symbol_(name))
+    end
+
+    symboltable = Dict{String, Symbol_}()
+    symbolT = symbol_("T")
+    symbolT.value = symbolT
+    symbolQuit = symbol_("QUIT")
+
+end # Symbols_
+
+
+
+end # JLLisp module
